3e62ced8fa382b4290585a3f0299132283221be6
EoinRaff <eoinrafferty@gmail.com>
2019-03-14 09:45:19 +0100

    added simple working Tilt script and GameManager with Singleton

---
+++ P8 - Life Under Water/Assets/GameManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        Singleton();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    private void Singleton()
+    {
+        if (instance == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ P8 - Life Under Water/Assets/Scripts/Tilt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour
+{
+    public Transform centerOfMass;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.Rotate(centerOfMass.localPosition);
+        Vector3 rotation = new Vector3();
+        rotation.x = centerOfMass.position.z;
+        rotation.z = -centerOfMass.position.x;
+        rotation.y = 0;
+
+        gameObject.transform.rotation = Quaternion.Euler(rotation);
+    }
+}
