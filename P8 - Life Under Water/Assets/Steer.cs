2d18d9418c2dc9a6d1e0065f680dafa810e621a6
EoinRaff <eoinrafferty@gmail.com>
2019-04-08 16:15:25 +0200

    kinectSteering

---
+++ P8 - Life Under Water/Assets/Steer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Steer : MonoBehaviour
+{
+    public Transform centerOfMass;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //gameObject.transform.Rotate(centerOfMass.localPosition);
+        Vector3 position = new Vector3();
+        position.x = centerOfMass.position.x;
+        position.z = 5; // -centerOfMass.position.x;
+        position.y = 0;
+        gameObject.transform.position = position;// = Quaternion.Euler(position);
+    }
+}
