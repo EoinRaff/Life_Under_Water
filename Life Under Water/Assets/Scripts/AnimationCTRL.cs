29c363162a9ed4192f483aff79cb9d4701bb20f5
ThomasLund17 <thomas.lund17@gmail.com>
2019-05-05 12:50:22 +0200

    audio and first animation timeline

---
+++ Life Under Water/Assets/Scripts/AnimationCTRL.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class AnimationCTRL : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Space) == true)
+        {
+            PlayAnimation();
+        }
+    }
+
+    public void PlayAnimation () 
+    {
+        playableDirector.Play();
+    }
+}
