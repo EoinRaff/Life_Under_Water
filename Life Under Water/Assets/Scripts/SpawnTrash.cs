49efd0cf1a0d28fedf6d3ad8f941d75ce425fbce
Anders Christiansen <aschristiansen@gmail.com>
2019-05-06 17:51:35 +0200

    Different sizes for textures and glow added
    When they are glowing they stand out more, and different sizes makes them more "realistic looking"

--- Life Under Water/Assets/Scripts/SpawnTrash.cs
+++ Life Under Water/Assets/Scripts/SpawnTrash.cs
@@ -34,29 +34,60 @@ public class SpawnTrash : MonoBehaviour
         StartCoroutine(ProduceTrash());
     }
 
+    // Where we spawn the trash
     IEnumerator ProduceTrash()
     {
-        float[,] trashLocations = new float[maxTrash, 2];
+        //float[,] trashLocations = new float[maxTrash, 2];
 
         while (trashCount <= maxTrash)
         {
             // Coordinates for where to spawn the next trash
             x = Random.Range(edge, width - edge);
             y = Random.Range(edge, height - edge);
-
-            trashLocations[trashCount, 0] = x;
-            trashLocations[trashCount, 1] = y;
+            
+            // For controlling trash coordinates by putting it in an array (currently unused) 
+            //trashLocations[trashCount, 0] = x;
+            //trashLocations[trashCount, 1] = y;
 
             // ---- Instantiate the trash
-            Trash.GetComponent<RawImage>().texture = trashTextures[Random.Range(0, trashTextures.Length - 1)]; // Give it a random texture
-            Trash.GetComponent<RawImage>().color = new Color(255, 255, 255, 255); // Start with alpha 0
-            Instantiate(Trash, new Vector3(x, y, 0), Quaternion.Euler(new Vector3(0,0,Random.Range(0,360))), gameObject.transform);
+            int p = Random.Range(0, trashTextures.Length); //To access the randomly chosen index of the array
+
+            currentTrash = Trash;
+
+            currentTrash.GetComponent<RawImage>().texture = trashTextures[p]; // Give it a random texture
+            currentTrash.GetComponent<RawImage>().color = new Color(255, 255, 255, 255); // Start with alpha 0 (for coroutine)
+
+            DetermineSize();
+
+            currentTrash = Instantiate(Trash, new Vector3(x, y, 0), Quaternion.Euler(new Vector3(0,0,Random.Range(0,360))), gameObject.transform) as GameObject;
 
             // Increase counter
             trashCount++;
 
             wait = Random.Range(lowerSpawnSpan, upperSpawnSpan);
             yield return new WaitForSeconds(wait);
+        }
+
+        // Set custom sizes for each piece of trash
+        void DetermineSize()
+        {
+            string n = currentTrash.GetComponent<RawImage>().texture.name;
+
+            switch (n)
+            {
+                case "group":
+                    currentTrash.transform.localScale = new Vector3(2.5f, 2.5f, 2.5f);
+                    print("Big trash spawned!");
+                    break;
+                case "bottle":
+                    currentTrash.transform.localScale = new Vector3(.5f, .5f, .5f);
+                    print("Bottle spawned!");
+                    break;
+                case "chips":
+                    currentTrash.transform.localScale = new Vector3(1, 1, 1);
+                    print("Chips spawned!");
+                    break;
+            }
         }
     }
 }
